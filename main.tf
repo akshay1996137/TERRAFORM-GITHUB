a1cf3da709cd03f7b00c21152c69e28bed546202
akshay1996137 <83005391+akshay1996137@users.noreply.github.com>
2026-01-21 11:45:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "TERRAFORM_HCP-1" {
+resource "aws_instance" "TERRAFORM_HCP" {
+  count = 3
   ami           = "ami-068c0051b15cdb816"
   instance_type = "t2.micro"
   tags = {
