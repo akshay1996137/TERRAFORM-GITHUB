c9c336ec5379aae91063261b957cc16ed5895143
akshay1996137 <83005391+akshay1996137@users.noreply.github.com>
2026-01-21 14:25:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "TERRAFORM_HCP" {
-  count = 1
+  count = 2
   ami           = "ami-068c0051b15cdb816"
-  instance_type = "t2.micro"
+  instance_type = "t2.large"
   tags = {
     Name = "TERRA_HCP"
   }
