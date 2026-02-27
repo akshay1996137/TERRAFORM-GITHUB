b3896225fe54339182e148eb1bb0b706296b3476
EC2 Default User <ec2-user@ip-172-31-23-74.ec2.internal>
2026-02-27 16:31:55 +0000

    added-file

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "three" {
+  ami           = "ami-0f3caa1cf4417e51b"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "MAP-SERVER"
+  }
+
+}
+
+
